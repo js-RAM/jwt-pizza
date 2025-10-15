86df1f854d2cea2c965b4e934b993c00d96078f9
js-RAM <swartzjoshua99@gmail.com>
2025-10-15 01:05:25 -0600

    updated tests to use mocks

--- tests/user.spec.ts
+++ tests/user.spec.ts
@@ -1,8 +1,84 @@
+import { Page } from '@playwright/test';
 import { test, expect } from 'playwright-test-coverage';
+import { Role, User } from '../src/service/pizzaService';
+
+async function basicInit(page: Page) {
+    let loggedInUser: User | undefined;
+    const validUsers: Record<string, User> = { 
+        'd@jwt.com': { id: '3', name: 'Kai Chen', email: 'd@jwt.com', password: 'a', roles: [{ role: Role.Diner }] },
+        'a@jwt.com': { id: '1', name: 'Admin', email: 'a@jwt.com', password: 'admin', roles: [{ role: Role.Admin }] },
+        'f@jwt.com': { id: '2', name: 'pizza franchise', email: 'f@jwt.com', password: 'franchisee', roles: [{ role: Role.Franchisee }] }
+    };
+
+    await page.route('*/**/api/auth', async (route) => {
+        if (route.request().method() == "PUT") {
+            const loginReq = route.request().postDataJSON();
+            const user = validUsers[loginReq.email];
+            if (!user || user.password !== loginReq.password) {
+                await route.fulfill({ status: 401, json: { error: 'Unauthorized' } });
+                return;
+            }
+            loggedInUser = validUsers[loginReq.email];
+            const loginRes = {
+                user: loggedInUser,
+                token: 'abcdef',
+            };
+            expect(route.request().method()).toBe('PUT');
+            await route.fulfill({ json: loginRes });
+        } else if (route.request().method() == "POST") {
+            const registerReq = route.request().postDataJSON();
+            const user = {
+                id: '4',
+                name: registerReq.name,
+                email: registerReq.email,
+                password: registerReq.password,
+                roles: [{ role: Role.Diner }]
+            };
+            validUsers[user.email] = user
+            const registerRes = {
+                user: user,
+                token: 'abcdef'
+            };
+            await route.fulfill({ json: registerRes });
+        } else if (route.request().method() == "DELETE") {
+            loggedInUser = undefined
+            await route.fulfill({ json: { message: "logout successful"} });
+        }
+    });
+
+    await page.route('*/**/api/user/4', async (route) => {
+        if (route.request().method() == "PUT") {
+            const req = route.request().postDataJSON();
+            validUsers[req.email].name = req.name;
+            if (req.password) validUsers[req.email].password = req.password;
+            loggedInUser = validUsers[req.email];
+            const loginRes = {
+                user: loggedInUser,
+                token: 'abcdef',
+            };
+            expect(route.request().method()).toBe('PUT');
+            await route.fulfill({ json: loginRes });
+        } else if (route.request().method() == "DELETE") {
+            delete validUsers['user4@jwt.com'];
+        }
+    });
+
+    await page.route(/\/api\/user(\?.*)?$/, async (route) => {
+        if (route.request().method() == "GET") {
+            const userRes = {
+                users: Object.values(validUsers),
+            };
+            expect(route.request().method()).toBe('GET');
+            await route.fulfill({ json: userRes });
+        }
+    });
+    await page.goto('/');
+}
+
 
 test('updateUser', async ({ page }) => {
+    await basicInit(page);
     const email = `user${Math.floor(Math.random() * 10000)}@jwt.com`;
-    await page.goto('/')
     await page.getByRole('link', { name: 'Register' }).click();
     await page.getByRole('textbox', { name: 'Full name' }).fill('pizza diner');
     await page.getByRole('textbox', { name: 'Email address' }).fill(email);
@@ -33,8 +109,8 @@ test('updateUser', async ({ page }) => {
 });
 
 test('changePassword', async ({ page }) => {
+    await basicInit(page);
     const email = `user${Math.floor(Math.random() * 10000)}@jwt.com`;
-    await page.goto('/')
     await page.getByRole('link', { name: 'Register' }).click();
     await page.getByRole('textbox', { name: 'Full name' }).fill('pizza diner');
     await page.getByRole('textbox', { name: 'Email address' }).fill(email);
@@ -61,7 +137,8 @@ test('changePassword', async ({ page }) => {
 });
 
 test('see UserList and delete', async ({ page }) => {
-    const email = `user${Math.floor(Math.random() * 10000)}@jwt.com`;
+    await basicInit(page);
+    const email = `user4@jwt.com`;
     const name = `user${Math.floor(Math.random() * 10000)}`
     await page.goto('/')
     await page.getByRole('link', { name: 'Register' }).click();
@@ -82,7 +159,7 @@ test('see UserList and delete', async ({ page }) => {
     await page.getByRole('textbox', { name: 'Filter users' }).fill(name);
     await page.getByRole('cell', { name: `${name} Submit` }).getByRole('button').click();
     await expect(page.getByRole('cell', { name: email })).toBeVisible();
-    await page.getByRole('button', { name: 'Delete' }).click();
+    await page.getByRole('row', { name: `${name} ${email}` }).getByRole('button').click();
     await page.getByRole('cell', { name: `${name} Submit` }).getByRole('button').click();
     await expect(page.getByRole('cell', { name: email })).not.toBeVisible();
 });
